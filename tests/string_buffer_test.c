41524f0c74b2bced1557e270dcd2a6b82e61c5e5
sagie gur ari <sagiegurari@gmail.com>
2020-04-17 22:13:58 +0000

    tests

--- tests/string_buffer_test.c
+++ tests/string_buffer_test.c
@@ -250,7 +250,26 @@ void test_flow()
   assert_num_equal(buffer->content_size, 31);
   assert_num_equal(buffer->max_size, 32);
   assert_string_equal(string_buffer_to_string(buffer), "12345abcdeABCDEtruefalse-123150");
-}
+
+  string_buffer_clear(buffer);
+  assert_num_equal(buffer->initial_size, 1);
+  assert_num_equal(buffer->content_size, 0);
+  assert_num_equal(buffer->max_size, 1);
+  assert_string_equal(string_buffer_to_string(buffer), "");
+
+  assert_true(string_buffer_append_string(buffer, "12345"));
+  assert_true(string_buffer_append_string(buffer, "abcde"));
+  assert_true(string_buffer_append_string(buffer, "ABCDE"));
+  assert_true(string_buffer_append_bool(buffer, true));
+  assert_true(string_buffer_append_bool(buffer, false));
+  assert_true(string_buffer_append_int(buffer, -123));
+  assert_true(string_buffer_append_long_long(buffer, 150));
+
+  assert_num_equal(buffer->initial_size, 1);
+  assert_num_equal(buffer->content_size, 31);
+  assert_num_equal(buffer->max_size, 32);
+  assert_string_equal(string_buffer_to_string(buffer), "12345abcdeABCDEtruefalse-123150");
+} /* test_flow */
 
 
 void fail()
