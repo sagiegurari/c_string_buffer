4c2335deb79cba3c1aba375b848fe8f2e8b9f5fe
sagie gur ari <sagiegurari@gmail.com>
2020-04-19 19:01:21 +0000

    ci

--- tests/test.c
+++ tests/test.c
@@ -3,6 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+#ifdef _WIN32
+#ifdef _WIN64
+#define SIZET_FORMAT    PRIu64
+#else
+#define SIZET_FORMAT    PRIu32
+#endif
+#else
+#define SIZET_FORMAT    "zu"
+#endif
+
 
 void test_run(void (*fn)(void))
 {
@@ -32,7 +42,7 @@ void assert_num_equal(size_t value1, size_t value2)
 {
   if (value1 != value2)
   {
-    printf("Assert Failed, value: %zu not equals to value: %zu", value1, value2);
+    printf("Assert Failed, value: %" SIZET_FORMAT " not equals to value: %" SIZET_FORMAT, value1, value2);
     test_fail();
   }
 }
