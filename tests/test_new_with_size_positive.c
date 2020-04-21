81d6ddbd2b6ccd15c3046341831b873a66cf8de7
sagie gur ari <sagiegurari@gmail.com>
2020-04-21 21:51:02 +0000

    debug windows

--- tests/test_new_with_size_positive.c
+++ tests/test_new_with_size_positive.c
@@ -7,8 +7,8 @@ void test_impl()
   const size_t        size    = 600;
   struct StringBuffer *buffer = string_buffer_new_with_options(size, true);
 
-  assert_true(string_buffer_is_empty(buffer));
-  //assert_true(string_buffer_is_allow_resize(buffer));
+  //assert_true(string_buffer_is_empty(buffer));
+  assert_true(string_buffer_is_allow_resize(buffer));
   //assert_num_equal(string_buffer_get_initial_size(buffer), size);
   //assert_num_equal(string_buffer_get_content_size(buffer), 0);
   //assert_num_equal(string_buffer_get_max_size(buffer), string_buffer_get_initial_size(buffer));
