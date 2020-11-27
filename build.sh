742a1ad3fad9d5968588414a832a3aaa6257733a
sagie gur ari <sagiegurari@gmail.com>
2020-11-27 15:01:20 +0000

    update build script

--- build.sh
+++ build.sh
@@ -10,7 +10,12 @@ echo Creating Target Directory...
 mkdir ./target
 
 cd ./target
-(cmake .. && make && ctest -C Release --output-on-failure) || true
-# memory check
-# ctest -T memcheck || true
+
+cmake .. && make && ctest -C Release --output-on-failure
+
+if [ "$1" = "--memcheck" ]; then
+  # memory check
+  ctest -T memcheck || true
+fi
+
 cd -
